8e987c85eba574b1982bb53536bffdba981eb75e
Harold Wong <harold.wong@microsoft.com>
2016-12-09 15:23:45 -0700

    Update file with latest installation actions

--- parts/extension-deis.sh
+++ parts/extension-deis.sh
@@ -5,41 +5,21 @@
 # Local variables
 
 export ADMIN_USERNAME=$1
-export MASTER_K8S_IP=$2
+export AZURE_STORAGE_ACCOUNT=$2
+export AZURE_STORAGE_ACCESS_KEY=$3
 
-# put in .profile
-echo 'export MASTER_K8S_IP='$MASTER_K8S_IP >> $HOME/.profile
-
-#run this command the first time to make sure keys are distributed
-scp -i ~/.ssh/id_rsa $ADMIN_USERNAME@$MASTER_K8S_IP:.kube/config ./$MASTER_K8S_IP.kubeconfig
-
-export KUBECONFIG=/home/$ADMIN_USERNAME/$MASTER_K8S_IP.kubeconfig
-echo 'export KUBECONFIG='$KUBECONFIG >> $HOME/.profile
-chmod 655 $MASTER_K8S_IP.kubeconfig 
-source $HOME/.profile
-
-#'install kubernetes client'
+# Install kubernetes client
 wget https://storage.googleapis.com/kubernetes-release/release/v1.4.4/bin/linux/amd64/kubectl
 chmod +x kubectl
-sudo mv kubectl /usr/local/bin/kubectl
+mv kubectl /usr/local/bin/kubectl
 
-#install deis
+# Install deis
 curl -sSL http://deis.io/deis-cli/install-v2.sh | bash
-sudo mv deis /usr/local/bin/deis
+mv deis /usr/local/bin/deis
 
-#install helm
+# Install helm
 curl -sSL https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get  | bash
 
-#install deis on kub cluster
+# Install deis on kub cluster
 helm init
-helm repo add deis https://charts.deis.com/workflow
-helm install deis/workflow --namespace deis
-
-#check progress of deis running
-kubectl --namespace=deis get pods
-
-echo "run this to see progress: kubectl --namespace=deis get pods"
-
-
-
-
+helm install deis/workflow --namespace=deis --set controller.docker_tag=v2.9.0-acs,controller.org=kmala,global.storage=azure,azure.accountname=$AZURE_STORAGE_ACCOUNT,azure.accountkey=$AZURE_STORAGE_ACCESS_KEY,azure.registry_container=registry,azure.database_container=database,azure.builder_container=builder
